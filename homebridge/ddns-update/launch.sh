aa7f55e28b542989f81df88235ffa061941639e8
thibaut1304 <thibaut.husser@free.fr>
2023-07-27 00:00:25 +0200

    dot env for srcipt launch

--- homebridge/ddns-update/launch.sh
+++ homebridge/ddns-update/launch.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+source .env
+
 if [ -z "$1" ]
 then
 	echo "1. Modifier config.json et lancer le container"
@@ -12,14 +14,14 @@ fi
 case $option in
 1)
 	sh $(pwd)/conf/startup.sh
-	docker build --no-cache -t ddns-update .
-	docker run -d --name=ddns-update -p 8000:8000 ddns-update
-	docker ps | grep ddns-update
+	docker build --no-cache -t $IMAGE_NAME .
+	docker run -d --name=$CONTAINER_NAME -p 8000:8000 $IMAGE_NAME
+	docker ps | grep $CONTAINER_NAME
 	sh $(pwd)/conf/startup.sh reverse
 	;;
 2)
-	docker rm -f ddns-update
-	docker rmi ddns-update
+	docker rm -f $CONTAINER_NAME
+	docker rmi $IMAGE_NAME
 	;;
 *)
 	echo "Option non valide, choisissez 1 ou 2."
