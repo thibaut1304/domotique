5a6de3f9f4e3aa2d66af410f09a4a3bb81af9c73
thibaut1304 <thibaut.husser@free.fr>
2023-07-28 12:23:52 +0200

    modif script http si meme ports

--- homebridge/proxy/script/start/port_config.sh
+++ homebridge/proxy/script/start/port_config.sh
@@ -4,28 +4,57 @@ echo "\n$COLOR_YELLOW""Get your port http and https ...""$COLOR_RESET"
 
 HTTP_PORTS=""
 HTTPS_PORTS=""
+declare -a unique_http_ports
+declare -a unique_https_ports
 
 while IFS= read -r i; do
 	HTTP_PORT=$(echo $i | jq '.http_port')
 	HTTPS_PORT=$(echo $i | jq '.https_port')
 
     if [ "$HTTP_PORT" != "null" ]; then
-		docker_command_run="${docker_command_run} -p ${HTTP_PORT}:${HTTP_PORT}"
-        if [ -z "$HTTP_PORTS" ]; then
+        if ! [[ " ${unique_http_ports[@]} " =~ " ${HTTP_PORT} " ]]; then
+            docker_command_run="${docker_command_run} -p ${HTTP_PORT}:${HTTP_PORT}"
+            unique_http_ports+=("$HTTP_PORT")
+			if [ -z "$HTTP_PORTS" ]; then
             HTTP_PORTS="${HTTP_PORT}"
         else
             HTTP_PORTS="${HTTP_PORTS}, ${HTTP_PORT}"
         fi
+        else
+            echo "Port ${HTTP_PORT} is already in use. Skipping..."
+        fi
     fi
+    # if [ "$HTTP_PORT" != "null" ]; then
+	# 	docker_command_run="${docker_command_run} -p ${HTTP_PORT}:${HTTP_PORT}"
+    #     if [ -z "$HTTP_PORTS" ]; then
+    #         HTTP_PORTS="${HTTP_PORT}"
+    #     else
+    #         HTTP_PORTS="${HTTP_PORTS}, ${HTTP_PORT}"
+    #     fi
+    # fi
 
     if [ "$HTTPS_PORT" != "null" ]; then
-		docker_command_run="${docker_command_run} -p ${HTTPS_PORT}:${HTTPS_PORT}"
-        if [ -z "$HTTPS_PORTS" ]; then
+        if ! [[ " ${unique_https_ports[@]} " =~ " ${HTTPS_PORT} " ]]; then
+            docker_command_run="${docker_command_run} -p ${HTTPS_PORT}:${HTTPS_PORT}"
+            unique_https_ports+=("$HTTPS_PORT")
+		if [ -z "$HTTPS_PORTS" ]; then
             HTTPS_PORTS="${HTTPS_PORT}"
         else
             HTTPS_PORTS="${HTTPS_PORTS}, ${HTTPS_PORT}"
         fi
+        else
+            echo "Port ${HTTPS_PORT} is already in use. Skipping..."
+        fi
     fi
+
+    # if [ "$HTTPS_PORT" != "null" ]; then
+	# 	docker_command_run="${docker_command_run} -p ${HTTPS_PORT}:${HTTPS_PORT}"
+    #     if [ -z "$HTTPS_PORTS" ]; then
+    #         HTTPS_PORTS="${HTTPS_PORT}"
+    #     else
+    #         HTTPS_PORTS="${HTTPS_PORTS}, ${HTTPS_PORT}"
+    #     fi
+    # fi
 	# if [ "$HTTP_PORT" != "null" ]; then
 	# 	docker_command_run="${docker_command_run} -p ${HTTP_PORT}:${HTTP_PORT}"
 	# fi
